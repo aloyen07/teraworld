b3645777926c7e8daa159e5bf37ab0f82ac4cde5
Aloyen <tolma4hova@gmail.con>
2023-12-27 09:14:45 +0300

    Initial ASM support

--- src/main/java/ru/aloyenz/teraworld/TargetFinder.java
+++ src/main/java/ru/aloyenz/teraworld/TargetFinder.java
@@ -1,26 +1,34 @@
 package ru.aloyenz.teraworld;
 
+import jdk.internal.org.objectweb.asm.ClassReader;
+import jdk.internal.org.objectweb.asm.tree.ClassNode;
 import net.minecraft.world.gen.IChunkGenerator;
 import org.reflections.Reflections;
 
-import java.util.ArrayList;
-import java.util.List;
+import java.io.IOException;
+import java.io.OutputStream;
+import java.util.HashSet;
 import java.util.Set;
+import java.io.InputStream;
 
 public class TargetFinder {
 
-    public static String[] findWorldGenClasses() {
+    public static String[] findWorldGenClasses() throws IOException {
         Set<Class<? extends IChunkGenerator>> classes =
                 new Reflections("").getSubTypesOf(IChunkGenerator.class);
 
-        List<String> clazzes = new ArrayList<>();
-        for (Class<? extends IChunkGenerator> clazz: classes) {
-            clazzes.add(clazz.toGenericString());
-        }
+        Set<ClassNode> classNodes = new HashSet<>();
 
-        String[] returns = clazzes.toArray(new String[clazzes.size()]);
-        for (String test: returns) {
-            System.out.println(test);
+        for (Class<? extends IChunkGenerator> clazz : classes) {
+
+            String className = clazz.getName();
+            String classAsPath = className.replace('.', '/') + ".class";
+            InputStream stream = TargetFinder.class.getClassLoader().getResourceAsStream(classAsPath);
+
+            ClassNode classNode = new ClassNode();
+            assert stream != null;
+            new ClassReader(stream).accept(classNode, ClassReader.SKIP_DEBUG);
+
         }
 
         return returns;
